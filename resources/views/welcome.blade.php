6f5d69a7fab1a610fdb56f7d9136bc6607287db0
Joseph Lebag <joseph.lebag28@gmail.com>
2023-01-12 17:06:45 +0400

    second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,10 +11,7 @@
             <div class="mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center h-15 bg-gradient-to-r from-blue-900 to-blue-900">
                 <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                     <!--Logo where to put-->
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-blue-500 rounded-full" viewBox="0 0 24 24">
-                        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                    </svg>
-                    <span class="ml-3 text-xl">TAPM</span>
+                    <span class="text-gray-50 ml-3 text-xl">TAPM</span>
                     </a>
 
                     <!--Links for log-in/sign-up-->
@@ -34,17 +31,12 @@
         <div class="bg-cover bg-[url('/img/apc-building.png')]">     
         <section class="text-gray-600 body-font">
             <div class="container px-5 py-24 mx-auto">
-                <div class="h-96 w-full bg-auto bg-center bg-no-repeat bg-[url('/img/logo.png')] mb-8">
-                    <!--<div class="flex flex-col text-center w-full mb-20">
-                    <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">TAPM LOGO HERE</h1>
-                    <p class="lg:w-2/3 mx-auto leading-relaxed text-base">About TAPM and Description Here.</p>
-                    </div>-->
-                </div>
+                <div class="h-screen pb-4 bg-top bg-auto bg-no-repeat bg-[url('/img/logo.png')]"></div>
                 <div class="flex flex-wrap -m-4">
                 <div class="lg:w-1/3 sm:w-1/2 p-4">
                     <div class="flex relative">
                     <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://dummyimage.com/600x360">
-                    <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                    <div class="px-8 py-10 relative z-10 w-full border-4 border-amber-500 bg-yellow-400 opacity-0 hover:bg-amber-500 opacity-100">
                         <h2 class="tracking-widest text-sm title-font font-medium text-blue-500 mb-1">Date/year</h2>
                         <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Title of Project</h1>
                         <p class="leading-relaxed">About the Project</p>
@@ -54,7 +46,7 @@
                 <div class="lg:w-1/3 sm:w-1/2 p-4">
                     <div class="flex relative">
                     <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://dummyimage.com/601x361">
-                    <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                    <div class="px-8 py-10 relative z-10 w-full border-4 border-amber-500 bg-yellow-400 opacity-0 hover:bg-amber-500 opacity-100">
                         <h2 class="tracking-widest text-sm title-font font-medium text-blue-500 mb-1">Date/year</h2>
                         <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Title of Project</h1>
                         <p class="leading-relaxed">About the Project</p>
@@ -64,7 +56,7 @@
                 <div class="lg:w-1/3 sm:w-1/2 p-4">
                     <div class="flex relative">
                     <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://dummyimage.com/603x363">
-                    <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                    <div class="px-8 py-10 relative z-10 w-full border-4 border-amber-500 bg-yellow-400 opacity-0 hover:bg-amber-500 opacity-100">
                         <h2 class="tracking-widest text-sm title-font font-medium text-blue-500 mb-1">Date/year</h2>
                         <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Title of Project</h1>
                         <p class="leading-relaxed">About the Project</p>
@@ -74,7 +66,7 @@
                 <div class="lg:w-1/3 sm:w-1/2 p-4">
                     <div class="flex relative">
                     <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://dummyimage.com/602x362">
-                    <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                    <div class="px-8 py-10 relative z-10 w-full border-4 border-amber-500 bg-yellow-400 opacity-0 hover:bg-amber-500 opacity-100">
                         <h2 class="tracking-widest text-sm title-font font-medium text-blue-500 mb-1">Date/year</h2>
                         <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Title of Project</h1>
                         <p class="leading-relaxed">About the Project</p>
@@ -84,7 +76,7 @@
                 <div class="lg:w-1/3 sm:w-1/2 p-4">
                     <div class="flex relative">
                     <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://dummyimage.com/605x365">
-                    <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                    <div class="px-8 py-10 relative z-10 w-full border-4 border-amber-500 bg-yellow-400 opacity-0 hover:bg-amber-500 opacity-100">
                         <h2 class="tracking-widest text-sm title-font font-medium text-blue-500 mb-1">Date/year</h2>
                         <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Title of Project</h1>
                         <p class="leading-relaxed">About the Project</p>
@@ -94,7 +86,7 @@
                 <div class="lg:w-1/3 sm:w-1/2 p-4">
                     <div class="flex relative">
                     <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://dummyimage.com/606x366">
-                    <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                    <div class="px-8 py-10 relative z-10 w-full border-4 border-amber-500 bg-yellow-400 opacity-0 hover:bg-amber-500 opacity-100">
                         <h2 class="tracking-widest text-sm title-font font-medium text-blue-500 mb-1">Date/year</h2>
                         <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Title of Project</h1>
                         <p class="leading-relaxed">About the Project</p>
